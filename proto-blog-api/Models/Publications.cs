a438e07637f0b7d8e8ea9e40e8e463678e46b354
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-12-10 21:03:08 -0300

    agregando config de DB

--- proto-blog-api/Models/Publications.cs
+++ proto-blog-api/Models/Publications.cs
@@ -1,6 +1,10 @@
 ﻿namespace proto_blog_api.Models
 {
-    public class publications
+    public class Publications
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public ICollection<Author> Authors { get; set; }
     }
 }
