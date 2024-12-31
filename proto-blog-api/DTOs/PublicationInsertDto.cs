7f4a9ab299913ce405778b7ca050e5915c0da09d
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-12-31 15:31:27 -0300

    config con jwt

--- proto-blog-api/DTOs/PublicationInsertDto.cs
+++ proto-blog-api/DTOs/PublicationInsertDto.cs
@@ -6,9 +6,11 @@ namespace proto_blog_api.DTOs
     {
         [Required(ErrorMessage ="El titulo es requerido")]
         public string Title { get; set; }
+        
         [Required(ErrorMessage ="El contenido es requerido")]
         public string Content { get; set; }
         public Boolean Deleted { get; set; }
+
         [Required(ErrorMessage ="debe incluir al menos un autor")]
         [MinLength(1,ErrorMessage ="debe incluir al menos un autor")]
         public ICollection<int> AuthorIds { get; set; } = new List<int>();
--- proto-blog-api/Program.cs
+++ proto-blog-api/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using proto_blog_api.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +12,36 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("StoreConection"));
 });
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuración de autenticación y autorización basada en JWT.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // configurar validacion del token
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true, //validar emisor del token
+            ValidateAudience = true, //validar audiencia (?
+            ValidateLifetime = true, // tiempo de vida
+            ValidateIssuerSigningKey = true,
+
+            // Aqui van los valores esperados del token
+            ValidIssuer = builder.Configuration["Jwt:Issuer"], // configuracion del emisor de appsettings.json
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]
+                ?? throw new InvalidOperationException("Jwt:Key is missing from configuration.")))
+
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
